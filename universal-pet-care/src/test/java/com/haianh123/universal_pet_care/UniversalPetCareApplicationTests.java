d421878e5f6977ddd2df079316acbe60b1e2937b
haianh123bg <nguyenngochaianh20033@gmail.com>
2024-08-10 22:30:08 +0700

    set up project

---
+++ universal-pet-care/src/test/java/com/haianh123/universal_pet_care/UniversalPetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haianh123.universal_pet_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalPetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
